569287c742b9e55476e8ba4f60c30fab16ad296f
Akho0122 <mr.akhror.dev@gmail.com>
2022-01-04 02:35:21 +0500

    About Me Components was styled

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,62 +1,69 @@
-import React, {useEffect} from 'react';
-import './AboutMe.css';
-import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading';
-import ScrollService from '../../Utilities/ScrollService';
-import Animations from '../../Utilities/Animations';
+import React, { useEffect } from "react";
+import "./AboutMe.css";
+import ScreenHeading from "../../Utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../Utilities/ScrollService";
+import Animations from "../../Utilities/Animations";
 const AboutMe = (props) => {
-    let fadeInScreenHandler = (screen)=>{
-        if(screen.fadeInScreen !== props.id)
-        return;
-        Animations.animations.fadeInScreen(props.id)
-    }
-    const fadeInSubscription = 
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-    const SCREEN_CONSTANTS = {
-        description: "Full stack web developer with background knowledge of Html, Css, Sass, Bootstrap, React.js along with utmost efficiency. Strong professional with Russian and English languages",
-        highlights: {
-            bullets: [
-                "Full Stack Web Development",
-                "Interactive Front End as per the design",
-                "React and React Native",
-                "Redux for State Management",
-                "Building REST API",
-                "Managing database",
-            ],
-            heading: "Here are a Few Highlights"
-        }
-    }
-    const renderHighlight = ()=>{
-        return (
-            SCREEN_CONSTANTS.highlights.bullets.map((value, i) =>(
-                <div className="highlight" key={i}>
-                    <div className="highlight-blob"></div>
-                    <span>{value}</span>
-                </div>
-            ))
-        )
-    }
-    return (
-        <div className="about-me-container screen-container" id={props.id || ""}>
-            <div className="about-me-parent">
-                <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"}/>
-                <div className="about-me-card">
-                    <div className="about-me-profile">
-
-                    </div>
-                    <div className="about-me-details">
-                        <span className="about-me-description">{SCREEN_CONSTANTS.description}</span>
-                        <div className="about-me-highlights">
-                            <div className="highlight-heading">
-                                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
-                            </div>
-                            {renderHighlight()}
-                        </div>
-                    </div>
-                </div>
+  const SCREEN_CONSTANTS = {
+    description:
+      "Full stack web developer with background knowledge of Html, Css, Sass, Bootstrap, React.js along with utmost efficiency. Strong professional with Russian and English languages",
+    highlights: {
+      bullets: [
+        "Full Stack Web Development",
+        "Interactive Front End as per the design",
+        "React and React Native",
+        "Redux for State Management",
+        "Building REST API",
+        "Managing database",
+      ],
+      heading: "Here are a Few Highlights",
+    },
+  };
+  const renderHighlight = () => {
+    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+  return (
+    <div className="about-me-container screen-container" id={props.id || ""}>
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-me-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+              </div>
+              {renderHighlight()}
             </div>
+            <div className="about-me-options">
+              <button className="btn primary-btn">
+                {""}
+                Hire Me{""}
+              </button>
+              <a href="my resume.pdf" download={"Akhrorbek's resume.pdf"}>
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
