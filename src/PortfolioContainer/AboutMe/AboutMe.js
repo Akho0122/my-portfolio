52c5c9a24aa1c20e29dc468020092ce3c029d57a
Akho0122 <mr.akhror.dev@gmail.com>
2022-01-04 01:31:55 +0500

    About Me component was added

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -9,11 +9,51 @@ const AboutMe = (props) => {
         return;
         Animations.animations.fadeInScreen(props.id)
     }
-    const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler)
+    const fadeInSubscription = 
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+    const SCREEN_CONSTANTS = {
+        description: "Full stack web developer with background knowledge of Html, Css, Sass, Bootstrap, React.js along with utmost efficiency. Strong professional with Russian and English languages",
+        highlights: {
+            bullets: [
+                "Full Stack Web Development",
+                "Interactive Front End as per the design",
+                "React and React Native",
+                "Redux for State Management",
+                "Building REST API",
+                "Managing database",
+            ],
+            heading: "Here are a Few Highlights"
+        }
+    }
+    const renderHighlight = ()=>{
+        return (
+            SCREEN_CONSTANTS.highlights.bullets.map((value, i) =>(
+                <div className="highlight" key={i}>
+                    <div className="highlight-blob"></div>
+                    <span>{value}</span>
+                </div>
+            ))
+        )
+    }
     return (
         <div className="about-me-container screen-container" id={props.id || ""}>
             <div className="about-me-parent">
                 <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"}/>
+                <div className="about-me-card">
+                    <div className="about-me-profile">
+
+                    </div>
+                    <div className="about-me-details">
+                        <span className="about-me-description">{SCREEN_CONSTANTS.description}</span>
+                        <div className="about-me-highlights">
+                            <div className="highlight-heading">
+                                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
+                            </div>
+                            {renderHighlight()}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
