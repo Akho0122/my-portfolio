ac27e811b9fda8b101313a68d13d6d03b949b1a6
Akho0122 <mr.akhror.dev@gmail.com>
2022-01-05 14:46:32 +0500

    ContactMe Form was added

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,49 +1,81 @@
-import React from 'react';
-import './ContactMe.css';
-import imgBack from '../../../src/images/mailz.jpeg';
-import load1 from '../../../src/images/load2.gif';
-import ScreenHeading from '../../Utilities/ScreenHeading/ScreenHeading';
-import ScrollService from '../../Utilities/ScrollService';
-import Animations from '../../Utilities/Animations';
-import Typical from 'react-typical';
+import React, { useState } from "react";
+import "./ContactMe.css";
+import imgBack from "../../../src/images/mailz.jpeg";
+import load1 from "../../../src/images/load2.gif";
+import ScreenHeading from "../../Utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../Utilities/ScrollService";
+import Animations from "../../Utilities/Animations";
+import Typical from "react-typical";
 
 const ContactMe = (props) => {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-    let fadeInScreenHandler = (screen) => {
-        if (screen.fadeInScreen !== props.id) return;
-        Animations.animations.fadeInScreen(props.id);
-      };
-      const fadeInSubscription =
-        ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
 
-    return (
-        <div className="main-container" id={props.id || ""}>
-            <ScreenHeading subHeading={"Lets Keep In Touch"} title={"Contact Me"}/>
-            <div className="central-form">
-                <div className="col">
-                <h2 className="title">
-                {""}
-                <Typical
-                  loop={Infinity}
-                  steps={[
-                    "Get In Touch 📧",
-                    1000,
-                  ]}
-                />
-              </h2>
-              <a href="https://www.instagram.com/akhrorbek.azamjonovich/">
-                <i className={"fa fa-instagram"}></i>
-              </a>
-              <a href="https://t.me/Akhrorbek0122">
-                <i className={"fa fa-telegram"}></i>
-              </a>
-              <a href="https://github.com/Akho0122?tab=overview&from=2022-01-01&to=2022-01-02">
-                <i className={"fa fa-github-square"}></i>
-              </a>
-                </div>
-            </div>
+  const handleName =(event) => {
+      setName(event.target.value);
+  }
+  
+  const handleEmail =(event) => {
+    setEmail(event.target.value);
+}
+
+const handleMessage =(event) => {
+    setMessage(event.target.value);
+}
+
+  return (
+    <div className="main-container" id={props.id || ""}>
+      <ScreenHeading subHeading={"Lets Keep In Touch"} title={"Contact Me"} />
+      <div className="central-form">
+        <div className="col">
+          <h2 className="title">
+            {""}
+            <Typical loop={Infinity} steps={["Get In Touch 📧", 1000]} />
+          </h2>
+          <a href="https://www.instagram.com/akhrorbek.azamjonovich/">
+            <i className={"fa fa-instagram"}></i>
+          </a>
+          <a href="https://t.me/Akhrorbek0122">
+            <i className={"fa fa-telegram"}></i>
+          </a>
+          <a href="https://github.com/Akho0122?tab=overview&from=2022-01-01&to=2022-01-02">
+            <i className={"fa fa-github-square"}></i>
+          </a>
         </div>
-    );
+        <div className="back-form">
+            <div className="img-back">
+                <h4>Send Your Email Here!</h4>
+                <img src={imgBack} alt="image not found" />
+            </div>
+            <form action="">
+                <p>{banner}</p>
+                <label htmlFor="name">Name</label>
+                <input type="text" onChange={handleName} value={name} />
+                <label htmlFor="email">Email</label>
+                <input type="email" onChange={handleEmail} value={email} />
+                <label htmlFor="message">Message</label>
+                <textarea type="text" onChange={handleMessage} value={message} />
+
+                <div className="send-btn">
+                    <button type="submit">
+                        send<i className="fa fa-paper-plane"/>
+                    </button>
+                </div>
+            </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactMe;
